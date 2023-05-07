dcfc1845783bcbc57970766cbe744d4374a9377f
KanzuWakazaki <KanzuWakazaki.Github@protonmail.com>
2023-05-07 19:18:07 +0700

    Update repl.nix

--- repl.nix
+++ repl.nix
@@ -1,8 +1,11 @@
 { pkgs }: {
 	deps = [
-		pkgs.nodejs-14_x
-		pkgs.nodePackages.typescript-language-server
-		pkgs.yarn
-		pkgs.replitPackages.jest
+  pkgs.nano
+  pkgs.nodejs-14_x
+  pkgs.nodePackages.typescript-language-server
+  pkgs.yarn
+  pkgs.replitPackages.jest
+  pkgs.libuuid
 	];
+  env = { LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];  };
 }
